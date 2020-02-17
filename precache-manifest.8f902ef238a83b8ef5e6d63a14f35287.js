b788424d5284515c331c486f067d379f025e415d
milan <milan387@gmail.com>
2020-02-17 00:28:34 -0600

    Updates

---
+++ precache-manifest.8f902ef238a83b8ef5e6d63a14f35287.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84f409e5b80b38335a4f07b83034720",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4b1eaf8646b7a645558",
+    "url": "/static/css/main.bef6aded.chunk.css"
+  },
+  {
+    "revision": "763182e05b63edcfbf34",
+    "url": "/static/js/2.1e089386.chunk.js"
+  },
+  {
+    "revision": "b4b1eaf8646b7a645558",
+    "url": "/static/js/main.acdb8e39.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
